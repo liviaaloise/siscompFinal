fad8ca7bb25620776623508a79be7ee848e309c3
liviaaloise <livia.aloise@gmail.com>
2017-11-12 16:51:25 -0200

    labf

--- ex3.c
+++ ex3.c
@@ -6,10 +6,8 @@
 #include <sys/shm.h>
 #include <sys/ipc.h>
 #include <sys/stat.h>
-#include <string.h>
-#define key 8901
-#define ever ;;
-#define MAX 16
+#include <wait.h>
+#define MAX 5
 union semun
 {
     int val;
@@ -29,52 +27,61 @@ int semaforoV(int semId);
 
 int main(){
     int seg, pid,semId,i;
-    char *a;
-    seg=shmget(IPC_PRIVATE,MAX*sizeof(char),IPC_CREAT | IPC_EXCL | S_IRUSR | S_IWUSR);
+    int *a;
+    
+    seg=shmget(IPC_PRIVATE,sizeof(int),IPC_CREAT | IPC_EXCL | S_IRUSR | S_IWUSR);
     
     if (seg == -1) {
         printf("Erro na criacao da memoria compartilhada");
         exit(1);
     }
-    a=(char*) shmat(seg,0,0);
-    a=0;
-    semId = semget (key, 1, 0666 | IPC_CREAT);
-    setSemValue(semId);
     
+    a=(int*) shmat(seg,0,0);
+    *a=0;
+    
+    semId = semget (IPC_PRIVATE, 1, 0666 | IPC_CREAT|IPC_EXCL);
+    setSemValue(semId);
     if ((pid = fork()) < 0) { /* criar processo */
         printf("Impossivel criar processo");
         exit(1);
     }
-    /* PAI - soma 5*/
+    
+    /* PAI -> soma 5*/
     else if(pid>0){
-        for (i=0;i<10;i++){
+        for (i=0;i<MAX;i++){
             semaforoP(semId);
             (*a)+=5;
             printf("5:  a=%d\n",*a);
-            semaforoV(semId);
-            sleep(1);
+            fflush(stdout);
+            semaforoV(semId);   
         }
     }
     
-    /*FILHO - soma 1*/
+    /*FILHO -> soma 1*/
     else{
-        for (i=0;i<10;i++){
+        for (i=0;i<MAX;i++){
             semaforoP(semId);
             (*a)++;
             printf("1:  a=%d\n",*a);
+            fflush(stdout);
             semaforoV(semId);
-            sleep(1);
+            
         }
+        if (shmdt(a) == -1) {
+		    printf("Erro no detach \n");
+		    exit(1);
+		}
     }
     
+    wait(NULL);
     
     if (shmdt(a) == -1) {
-        printf("Erro no detach");
+        printf("Erro no detach \n");
         exit(1);
     }
     
     if (shmctl(seg, IPC_RMID, NULL) == -1) {
-        printf("Erro ao liberar memoria compartilhada");
+        printf("Erro ao liberar memoria compartilhada\n");
     }
     
     delSemValue(semId);
