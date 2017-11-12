25dbfc7288b0663b8812a55e13d25a4b4b8e4fd2
liviaaloise <livia.aloise@gmail.com>
2017-11-12 16:38:35 -0200

    Add files via upload

--- ex4a.c
+++ ex4a.c
@@ -1,19 +1,38 @@
-#include <stdio.h>
+#include <sys/sem.h>
 #include <unistd.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 #include <sys/shm.h>
 #include <sys/ipc.h>
 #include <sys/stat.h>
 #include <string.h>
 #define MAX 150
-#define key 8752
+#define key 9005
+#define key2 8889
+union semun
+{
+    int val;
+    struct semid_ds *buf;
+    ushort *array;
+};
+// inicializa o valor do semáforo
+int setSemValue(int semId);
+// remove o semáforo
+void delSemValue(int semId);
+// operação P
+int semaforoP(int semId);
+//operação V
+int semaforoV(int semId);
+
+
+
 int main()
 {
-    int seg1;
+    int seg1,semId,i;
     char men[MAX-1],*p;
     
-    seg1 = shmget(key,MAX*sizeof(char),IPC_CREAT | IPC_EXCL | S_IRUSR | S_IWUSR);
-    
+    seg1= shmget(key,MAX*sizeof(char),IPC_CREAT | IPC_EXCL | S_IRUSR | S_IWUSR);
     if (seg1==-1){
         printf("Memoria compartilhada nao criada");
         exit(1);
@@ -24,11 +43,16 @@ int main()
         printf("Erro no attach");
         exit(1);
     }
-    printf("Mensagem do dia: ");
-    scanf(" %149[^\n]", men);
+	semId = semget (key2, 1, 0666 | IPC_CREAT);
+    setSemValue(semId);
     
-    strcpy(p,men);
-    
+	for (i=0;i<10;i++){
+		semaforoP(semId);
+		printf("Mensagem do dia: ");
+		scanf(" %149[^\n]", men);
+		strcpy(p,men);
+		semaforoV(semId);
+    }
     if(shmdt(p)==-1){
         printf("Erro no detach");
         exit(1);
@@ -36,3 +60,37 @@ int main()
 
     return 0;
 }
+
+
+int setSemValue(int semId)
+{
+    union semun semUnion;
+    semUnion.val = 1;
+    return semctl(semId, 0, SETVAL, semUnion);
+}
+
+void delSemValue(int semId)
+{
+    union semun semUnion;
+    semctl(semId, 0, IPC_RMID, semUnion);
+}
+
+int semaforoP(int semId)
+{
+    struct sembuf semB;
+    semB.sem_num = 0;
+    semB.sem_op = -1;
+    semB.sem_flg = SEM_UNDO;
+    semop(semId, &semB, 1);
+    return 0;
+}
+int semaforoV(int semId)
+{
+    struct sembuf semB;
+    semB.sem_num = 0;
+    semB.sem_op = 1;
+    semB.sem_flg = SEM_UNDO;
+    semop(semId, &semB, 1);
+    return 0;
+}
+
--- ex4b.c
+++ ex4b.c
@@ -1,17 +1,36 @@
-
-#include <stdio.h>
+#include <sys/sem.h>
 #include <unistd.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 #include <sys/shm.h>
 #include <sys/ipc.h>
 #include <sys/stat.h>
 #include <string.h>
 #define MAX 150
-#define key 8752
+#define key 9005
+#define key2 8889
+
+union semun
+{
+    int val;
+    struct semid_ds *buf;
+    ushort *array;
+};
+// inicializa o valor do semáforo
+int setSemValue(int semId);
+// remove o semáforo
+void delSemValue(int semId);
+// operação P
+int semaforoP(int semId);
+//operação V
+int semaforoV(int semId);
+
+
 
 int main()
 {
-    int seg1;
+    int seg1,i,semId;
     char *p;
     
     seg1 = shmget(key,MAX*sizeof(char), S_IRUSR | S_IWUSR);
@@ -25,9 +44,18 @@ int main()
         printf("Erro no attach\n");
         exit(1);
     }
-    printf("%s",p);
-    printf("\n");
     
+	semId = semget (key2, 1, 0666);
+    setSemValue(semId);
+
+
+	for (i=0;i<10;i++){
+		//sleep(3);
+		semaforoP(semId);
+		printf("%s",p);
+		printf("\n");
+		semaforoV(semId);
+    }
     if(shmdt(p)==-1){
         printf("Erro no detach\n");
         exit(1);
@@ -40,3 +68,36 @@ int main()
  
     return 0;
 }
+
+int setSemValue(int semId)
+{
+    union semun semUnion;
+    semUnion.val = 1;
+    return semctl(semId, 0, SETVAL, semUnion);
+}
+
+void delSemValue(int semId)
+{
+    union semun semUnion;
+    semctl(semId, 0, IPC_RMID, semUnion);
+}
+
+int semaforoP(int semId)
+{
+    struct sembuf semB;
+    semB.sem_num = 0;
+    semB.sem_op = -1;
+    semB.sem_flg = SEM_UNDO;
+    semop(semId, &semB, 1);
+    return 0;
+}
+int semaforoV(int semId)
+{
+    struct sembuf semB;
+    semB.sem_num = 0;
+    semB.sem_op = 1;
+    semB.sem_flg = SEM_UNDO;
+    semop(semId, &semB, 1);
+    return 0;
+}
+
